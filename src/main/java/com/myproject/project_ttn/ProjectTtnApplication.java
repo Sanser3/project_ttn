d7f8bedf01cbd6022c7193b168a1b61f057bb5e2
Sanser3 <98323544+Sanser3@users.noreply.github.com>
2023-05-27 08:16:03 +0200

    Initial commit

---
+++ src/main/java/com/myproject/project_ttn/ProjectTtnApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.project_ttn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTtnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTtnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/project_ttn/ProjectTtnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.project_ttn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTtnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
